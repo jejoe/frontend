9c5f08bd6181821cb83331ec88521416ec4fce3d
Jeni Joe <jenijoe@Jenis-MacBook-Pro.local>
2020-06-22 13:54:47 -0500

    temp

--- src/App.js
+++ src/App.js
@@ -12,28 +12,24 @@ function App() {
       <div className='d-flex flex-column'>
         <div className='d-flex justify-content-between pr-5 pl-5 pt-3 pb-3' style={{ backgroundColor: 'alice', boxShadow: '0 1px 3px rgba(83,91,96,.23)' }}>
           <div className='d-flex'>
-            <Link to='/' style={{ fontSize:'30px'}}>GO TO MARKET</Link>
+            <Link to='/' style={{ fontSize: '30px' }}>GO TO MARKET</Link>
           </div>
           <div className='d-flex'>
             <div className='d-flex'>
-            <div className='mr-3'>
-                <Link to='/' style={{ fontSize:'20px'}}>Filter and Export</Link>
-              </div>
               <div className='mr-3'>
-                <Link to='/' style={{ fontSize:'20px'}}>Deals List</Link>
+                <Link to='/' style={{ fontSize: '20px' }}>Deals List</Link>
               </div>
               <div>
-                <Link to='/create' style={{ fontSize:'20px'}}>Create Deal</Link>
+                <Link to='/create' style={{ fontSize: '20px' }}>Create Deal</Link>
               </div>
             </div>
           </div>
         </div>
         <div className='pr-5 pl-5'>
-        <Route path='/' exact component={DealsList} />
-        <Route path='/edit/:id' component={EditDeal} />
-        <Route path='/delete/:id' component={DeleteDeal} />
-        <Route path='/create' component={CreateDeal} />
-        {/* <Route path='/export' component={ExportDeal} /> */}
+          <Route path='/' exact component={DealsList} />
+          <Route path='/edit/:id' component={EditDeal} />
+          <Route path='/delete/:id' component={DeleteDeal} />
+          <Route path='/create' component={CreateDeal} />
         </div>
       </div>
     </Router>
